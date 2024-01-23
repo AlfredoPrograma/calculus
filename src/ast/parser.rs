23535234c9f00a49c9f1b74d5628c8da101032db
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-01-23 06:16:13 -0400

    test: add tests for expressions parsing in ast parser
    implement test for literal expression
    implement test for unary expression
    implement test for factor expression
    implement test for term expression

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -26,15 +26,15 @@ impl<I: Iterator<Item = Token> + Clone + Debug> Parser<I> {
 impl<I: Iterator<Item = Token> + Clone + Debug> Parser<I> {
     /// Builds the root's program expression.
     ///   
-    /// Production rule: `Program -> Expression`
+    /// Production rule: `Program -> (Term)*`
     pub fn program(&mut self) -> Expression {
-        self.expression()
+        self.term()
     }
 
-    /// Builds an expression.
+    /// Builds a term.
     ///
-    /// Production rule: `Expression -> Factor (("+" | "-") Factor)*`
-    fn expression(&mut self) -> Expression {
+    /// Production rule: `Term -> Factor (("+" | "-") Factor)*`
+    fn term(&mut self) -> Expression {
         const TERM_OPERATORS: &[Token] = &[
             Token::Operator(Operator::Plus),
             Token::Operator(Operator::Minus),
@@ -107,7 +107,7 @@ impl<I: Iterator<Item = Token> + Clone + Debug> Parser<I> {
 
     /// Builds an unary.
     ///
-    /// Production rule: `"-" Literal`
+    /// Production rule: `"-" Literal | Literal`
     fn unary(&mut self) -> Expression {
         match peek(&mut self.tokens) {
             Some(token) => match token {
@@ -123,7 +123,7 @@ impl<I: Iterator<Item = Token> + Clone + Debug> Parser<I> {
                 }
                 Token::Number(_) => self.literal(),
             },
-            None => panic!("end of input"),
+            None => panic!("uncomplete of input"),
         }
     }
 
@@ -138,5 +138,184 @@ impl<I: Iterator<Item = Token> + Clone + Debug> Parser<I> {
         }
 
         panic!("unexpected token")
+    }
+}
+
+#[cfg(test)]
+mod ast_parser_tests {
+
+    use crate::{
+        ast::expressions::{BinaryExpr, Expression, UnaryExpr},
+        tokenizer::tokens::{Operator, Token},
+    };
+
+    use super::Parser;
+
+    #[test]
+    fn test_literal_success() {
+        // Arrange
+        let literal_token = Token::Number(10.0);
+        let tokens_source = [literal_token.clone()].into_iter();
+
+        let mut parser = Parser::new(tokens_source);
+        let expected_expr = Expression::Literal(literal_token);
+
+        // Act
+        let literal_expr = parser.literal();
+
+        // Assert
+        assert_eq!(
+            literal_expr, expected_expr,
+            "should build literal expression for given token"
+        );
+
+        assert_eq!(
+            parser.tokens.count(),
+            0,
+            "should consume token from tokens source once expression was built"
+        )
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_literal_fails() {
+        // Arrange
+        let non_literal_token = Token::Operator(Operator::Star);
+        let tokens_source = [non_literal_token].into_iter();
+        let mut parser = Parser::new(tokens_source);
+
+        // Act & Assert
+        // Notice we are trying to create a literal from a non literal token (Operator), so function panics.
+        parser.literal();
+    }
+
+    #[test]
+    fn test_unary_with_operator_success() {
+        // Arrange
+        let operator_token = Token::Operator(Operator::Minus);
+        let literal_token = Token::Number(25.5);
+
+        let tokens_source = [operator_token.clone(), literal_token.clone()].into_iter();
+        let mut parser = Parser::new(tokens_source);
+
+        let expected_expr = Expression::Unary(UnaryExpr::new(
+            operator_token,
+            Expression::Literal(literal_token),
+        ));
+
+        // Act
+        let unary_expr = parser.unary();
+
+        // Assert
+        assert_eq!(
+            expected_expr, unary_expr,
+            "should build unary expression for a valid stream of tokens"
+        );
+    }
+
+    #[test]
+    fn test_unary_for_literal_success() {
+        // Arrange
+        let literal_token = Token::Number(29.9);
+        let tokens_source = [literal_token.clone()].into_iter();
+
+        let mut parser = Parser::new(tokens_source);
+        let expected_expr = Expression::Literal(literal_token);
+
+        // Act
+        let literal_from_unary = parser.unary();
+
+        // Assert
+        assert_eq!(
+            expected_expr, literal_from_unary,
+            "should build a literal expression for given tokens stream if operator does not exist"
+        )
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_unary_fails_by_invalid_operator() {
+        // Arrange
+        let non_unary_operator = Token::Operator(Operator::Star);
+        let literal_token = Token::Number(99.9);
+        let tokens_source = [non_unary_operator.clone(), literal_token.clone()].into_iter();
+
+        let mut parser = Parser::new(tokens_source);
+
+        // Act & Assert
+        // Notice it should panic because star operator is not a valid operator to perform unary expression building
+        parser.unary();
+    }
+
+    #[test]
+    fn test_factor_success() {
+        // Arrange
+        let left_literal = Token::Number(10.0);
+        let right_literal = Token::Number(20.0);
+        let factor_operators = [
+            Token::Operator(Operator::Star),
+            Token::Operator(Operator::Slash),
+        ];
+
+        for operator in factor_operators {
+            let tokens_source = [
+                left_literal.clone(),
+                operator.clone(),
+                right_literal.clone(),
+            ]
+            .into_iter();
+
+            let mut parser = Parser::new(tokens_source);
+            let expected_expr = Expression::Binary(BinaryExpr::new(
+                Expression::Literal(left_literal.clone()),
+                operator,
+                Expression::Literal(right_literal.clone()),
+            ));
+
+            // Act
+            let factor_expr = parser.factor();
+
+            // Assert
+            assert_eq!(
+                factor_expr, expected_expr,
+                "should build a binary expression from factor production rule"
+            )
+        }
+    }
+
+    #[test]
+    fn test_expression_success() {
+        // Arrange
+        let left_literal = Token::Number(10.0);
+        let right_literal = Token::Number(20.0);
+        let factor_operators = [
+            Token::Operator(Operator::Plus),
+            Token::Operator(Operator::Minus),
+        ];
+
+        for operator in factor_operators {
+            let tokens_source = [
+                left_literal.clone(),
+                operator.clone(),
+                right_literal.clone(),
+            ]
+            .into_iter();
+
+            let mut parser = Parser::new(tokens_source);
+            let expected_expr = Expression::Binary(BinaryExpr::new(
+                Expression::Literal(left_literal.clone()),
+                operator,
+                Expression::Literal(right_literal.clone()),
+            ));
+
+            // Act
+            let factor_expr = parser.term();
+
+            // Assert
+            assert_eq!(
+                factor_expr, expected_expr,
+                "should build a binary expression from factor production rule"
+            )
+        }
     }
 }
